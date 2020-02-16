a43f0c138bc166e84452c43225da70fad3fc597f
Rodrigo Botti <rodrigo.botti@gmail.com>
2020-02-16 19:23:37 -0300

    Feature: brick movements and tests

--- lib/brick.ex
+++ lib/brick.ex
@@ -1,23 +1,49 @@
 defmodule Tetris.Brick do
-  defstruct [
-    name: :i,
-    location: {40, 0},
-    rotation: 0,
-    reflection: false
-  ]
+  defstruct name: :i,
+            location: {40, 0},
+            rotation: 0,
+            reflection: false
+
+  alias __MODULE__, as: Brick
 
   @spec new :: Tetris.Brick.t()
-  def new, do: %__MODULE__{}
+  def new, do: %Brick{}
 
   @spec new_random :: Tetris.Brick.t()
   def new_random do
-    %__MODULE__{
+    %Brick{
       name: random_name(),
       location: {40, 0},
       rotation: random_rotation(),
       reflection: random_reflection()
     }
   end
+
+  @spec down(Tetris.Brick.t()) :: Tetris.Brick.t()
+  def down(brick) do
+    %Brick{brick | location: point_down(brick.location)}
+  end
+
+  @spec left(Tetris.Brick.t()) :: Tetris.Brick.t()
+  def left(brick) do
+    %Brick{brick | location: point_left(brick.location)}
+  end
+
+  @spec right(Tetris.Brick.t()) :: Tetris.Brick.t()
+  def right(brick) do
+    %Brick{brick | location: point_right(brick.location)}
+  end
+
+  def spin_90(brick) do
+    %Brick{brick | rotation: rotate(brick.rotation)}
+  end
+
+  defp point_down({x, y}), do: {x, y + 1}
+  defp point_left({x, y}), do: {x - 1, y}
+  defp point_right({x, y}), do: {x + 1, y}
+
+  defp rotate(270), do: 0
+  defp rotate(degrees), do: degrees + 90
 
   defp random_name do
     ~w(i l z o t)a
@@ -33,5 +59,4 @@ defmodule Tetris.Brick do
     [0, 90, 180, 270]
     |> Enum.random()
   end
-
 end
--- test/brick_test.exs
+++ test/brick_test.exs
@@ -1,18 +1,32 @@
 defmodule BrickTest do
   use ExUnit.Case
 
-  alias Tetris.Brick
+  import Tetris.Brick
 
   test "Creates a new brick" do
-    assert Brick.new().name == :i
+    assert new().name == :i
   end
 
   test "Creates a new random brick" do
-    brick = Brick.new_random()
+    brick = new_random()
 
     assert brick.name in [:i, :l, :z, :o, :t]
     assert brick.rotation in [0, 90, 180, 270]
     assert brick.reflection in [true, false]
+    assert brick.location == {40, 0}
   end
 
+  test "Should manipulate brick" do
+    brick =
+      new()
+      |> left()
+      |> right()
+      |> down()
+      |> spin_90()
+
+    assert brick.name in [:i, :l, :z, :o, :t]
+    assert brick.rotation == 90
+    assert brick.reflection == false
+    assert brick.location == {40, 1}
+  end
 end
